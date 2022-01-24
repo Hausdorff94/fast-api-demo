4c9e01b9060fc4067af9e8e3d509f1135e828ad7
Hausdorff94 <jhonyal58@gmail.com>
2022-01-23 19:04:28 -0500

    Update main

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-#Python
+# Python
 from email.policy import default
+# Enum
+from enum import Enum
 from typing import Optional
 
-#Pydantic
-from pydantic import BaseModel
-from pydantic import Field
-
-#Enum
-from enum import Enum
-
-#FastAPI
-from fastapi import FastAPI
-from fastapi import Body, Query, Path
+# FastAPI
+from fastapi import Body, FastAPI, Path, Query
+# Pydantic
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-#Models
+# Models
 
 class HairColor(Enum):
     BLONDE = "blonde"
@@ -25,10 +21,12 @@ class HairColor(Enum):
     GREY = "grey"
     WHITE = "white"
 
+
 class Location(BaseModel):
     city: str
     state: str
     country: str
+
 
 class Person(BaseModel):
     first_name: str = Field(
@@ -37,25 +35,24 @@ class Person(BaseModel):
         description="The person's first name",
         max_length=50,
         min_length=2
-        )
+    )
     last_name: str = Field(
         ...,
         title="Last Name",
         description="The person's last name",
         max_length=50,
         min_length=2
-        )
+    )
     age: int = Field(
         ...,
         title="Age",
         description="The person's age",
         gt=0,
         lt=115
-        )
+    )
     hair_color: Optional[HairColor] = Field(default=None)
-    is_married: Optional[bool] = Field(
-        default=None
-        )
+    is_married: Optional[bool] = Field(default=None)
+    password: str = Field(..., min_length=8)
 
     class Config:
         schema_extra = {
@@ -68,17 +65,20 @@ class Person(BaseModel):
             }
         }
 
+
 @app.get("/")
 def home():
     return {"message": "Hello World"}
 
 # Request and Response body
 
+
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
 
 # Validations: Query parameters
+
 
 @app.get("/person/detail/")
 def show_person(
@@ -89,17 +89,18 @@ def show_person(
         title='Person Name',
         description='This is the name of the person you are looking for. Its length must be between 1 and 50 characters.',
         example='Karla'
-        ),
+    ),
     age: str = Query(
         ...,
-        title= 'Person Age',
+        title='Person Age',
         description='This is the age of the person you are looking for. Its required.',
         example=33
-        ),
+    ),
 ):
     return {"name": name, "age": age}
 
 # Validations: Path parameters
+
 
 @app.get("/person/detail/{person_id}")
 def show_person(
@@ -109,25 +110,26 @@ def show_person(
         title='Person ID',
         description='This is the ID of the person you are looking for. Its required and must be greater than 0.',
         example=1
-        ),
+    ),
 ):
     return {"person_id": person_id}
 
 # Validations: Request body
 
+
 @app.put("/person/{person_id}")
 def update_person(
-   person_id: int = Path(
-       ...,
-       gt=0,
-       title='Person ID',
-       description='This is the ID of the person you are looking for. Its required and must be greater than 0.',
-       example=132
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title='Person ID',
+        description='This is the ID of the person you are looking for. Its required and must be greater than 0.',
+        example=132
     ),
-   person: Person = Body(
-       ...,
-       title='Person',
-       description='This is the person you are looking for. Its required.',
+    person: Person = Body(
+        ...,
+        title='Person',
+        description='This is the person you are looking for. Its required.',
     ),
     Location: Location = Body(
         ...,
